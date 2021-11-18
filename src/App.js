f4a0fef1d4ab7982cf017969bc1c04c694badc49
enriquedfam <enriquedfa@gmail.com>
2021-11-18 11:50:03 -0600

    Folder rename

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Homepage from './components/Homepage/Homepage';
+import Header from './components/Head/Header';
+import Footer from './components/Foot/Footer';
+import Homepage from './components/Home/Homepage';
 
 function App() {
   return (
