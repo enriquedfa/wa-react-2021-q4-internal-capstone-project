c7c5d3b8a909858aa0e6d0c308967b6a65a9c037
enriquedfam <enriquedfa@gmail.com>
2021-12-05 23:09:25 -0600

    Separated card from categories

--- src/components/Categories/Categories.component.jsx
+++ src/components/Categories/Categories.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useCategories } from "../../utils/hooks/useCategories";
+import Card from "../Card.component";
 
 function Categories() {
   const { data: catList, isLoading: catIsLoading } = useCategories();
@@ -13,16 +13,7 @@ function Categories() {
       ) : (
         <div className="categories-grid">
           {catList.results.map((category) => (
-            <div className="category" key={category.id}>
-              <Link to={`/products?category=${category.id}`}>
-                <img
-                  className="category-image"
-                  src={category.data.main_image.url}
-                  alt={category.data.main_image.alt}
-                />
-                <h3>{category.data.name}</h3>
-              </Link>
-            </div>
+            <Card {...category} />
           ))}
         </div>
       )}
