6ca0c62bc1df3e3cfda659a465038a1e4b8b9de5
enriquedfam <enriquedfa@gmail.com>
2021-12-10 13:56:01 -0600

    removed imports

--- src/components/Products/Products.component.jsx
+++ src/components/Products/Products.component.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import { React } from "react";
 import Product from "../Product/Product.component";
 import ReactPaginate from "react-paginate";
 
